aad0202665482044aa11e33dcc36dbaf31517c45
CesarCejaC <tonycejacorte@gmail.com>
2022-03-03 08:50:58 -0600

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import {PagesModule} from "./pages.module";
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { LayoutModule } from './layout/layout.module';
   ],
   imports: [
     BrowserModule,
-    LayoutModule
-  
+    LayoutModule,
+    PagesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages.module.ts
+++ src/app/pages.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HomePageModule } from './pages/home-page/home-page.module';
 import { CommonModule } from '@angular/common';
+import {CommunityPageModule} from "./pages/community-page/community-page.module";
+import {ReviewPageModule} from "./pages/review-page/review-page.module";
 @NgModule({
     declarations:[],
     imports:[
         CommonModule,
         HomePageModule,
-    ]    
+        CommunityPageModule,
+        ReviewPageModule,
+    ]
 })
 export class PagesModule {}
